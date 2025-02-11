b710a4cb0a9a77ba3adccea8246d1e83de437d48
anthonyss09 <76715818+anthonyss09@users.noreply.github.com>
2025-02-11 17:53:45 -0500

    update: style updates

--- client/src/app/componets/Header.tsx
+++ client/src/app/componets/Header.tsx
@@ -3,14 +3,16 @@ import BtnContMain from "./BtnContMain";
 
 export default function Header() {
   return (
-    <header className="w-[352px] z-10 relative flex flex-col mt-[64px] mx-auto py-8 xs:mt-[120px]  xs:w-[440px] md:border-[1px] md:bg-white/60 md:border-greyLight/10 md:shadow-lg md:flex-row md:items-center md:justify-center md:gap-6 md:w-[768px] md:h-[560px] md:mt-24 md:py-40 md:border-0 lg:w-[90%]">
-      <div className="md:h-full md:flex md:flex-col md:justify-start md:gap-8">
+    <header className="w-[352px] z-10 relative flex flex-col mt-[64px] mx-auto py-8 xs:mt-[120px]  xs:w-[440px] md:border-[1px] md:bg-white/60 md:border-greyLight/10 md:shadow-lg md:flex-row md:items-center md:justify-center md:gap-6 md:w-[768px] md:h-[560px] md:mt-24 md:py-[120px] md:border-0 lg:w-[90%]">
+      <div className="md:h-full md:flex md:flex-col md:justify-start md:gap-4">
         {" "}
-        <ProfilePic />
+        <div className="flex justify-start items-end gap-4 text-[32px] xs:ml-12 md:ml-0">
+          {" "}
+          <ProfilePic /> <p>Hi.</p>
+        </div>
         <h1
-          className={`my-4 mt-8 ml-4 font-[400] text-[32px] leading-[34px] xs:ml-16 md:m-0`}
+          className={`my-4 mt-4 ml-4 font-[400] text-[32px] leading-[34px] xs:ml-16 md:m-0 md:leading-[48px]`}
         >
-          <p>Hi.</p>
           <p>I&apos;m Anthony Toffetti.</p>
           <p>A Web Developer.</p>
         </h1>
@@ -19,7 +21,7 @@ export default function Header() {
       <div className="md:h-full md:flex md:flex-col md:justify-end md:gap-8 md:w-[336px]">
         {" "}
         <div
-          className={`my-4 mb-8 ml-4 py-4  text-[20px] font-[400] bg-white/60 leading-[26px] xs:w-[336px] xs:ml-16 md:m-0  md:py-8 md:px-4 md:shadow-md`}
+          className={`w-[298px] my-4 mb-8 ml-4 py-4  text-[20px] font-[400] bg-white/60 leading-[26px] md:w-[336px] xs:ml-16 md:m-0  md:py-8 md:px-4 md:shadow-md`}
         >
           <p>I write clean, developer friendly</p>
           <p></p>
